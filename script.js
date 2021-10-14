fc5519f0e0466bfc9b03790ca10ed89976db28fc
daotunglam <lam90namdinh@gmail.com>
2021-10-14 19:53:26 +0200

    Add function loadNext20Pkms

--- script.js
+++ script.js
@@ -5,16 +5,16 @@
  *      document.getElementById(`pkmCard${i}`).style.backgroundColor = type;
  * - to add an event 'click' in pkmCard with a function WITH PARAMETERS, I've found no way.
  *      this doesn't work:
- *          document.getElementById(`pkmCard(${i})`).addEventListener("click", makeBig(this, pkmJson, i) )
+ *          document.getElementById(`pkmCard(${i})`).addEventListener("click", makeBig(this, allPokemons, i) )
  *      this doesn't work:
- *          document.getElementById(`pkmCard(${i})`).addEventListener("click", makeBig.bind(this, pkmJson, i) )
+ *          document.getElementById(`pkmCard(${i})`).addEventListener("click", makeBig.bind(this, allPokemons, i) )
  *      this doesn't work:
- *          document.getElementById(`pkmCard(${i})`).onclick = makeBig(this, pkmJson, i)
+ *          document.getElementById(`pkmCard(${i})`).onclick = makeBig(this, allPokemons, i)
  *      this doesn't work:
- *          document.getElementById(`pkmCard(${i})`).onclick = function() {makeBig(this, pkmJson, i)}
+ *          document.getElementById(`pkmCard(${i})`).onclick = function() {makeBig(this, allPokemons, i)}
  *      this doesn't work:
  *          document.getElementById(`pkmCard(${i})`).addEventListener("click", function(){
- *              makeBig(this, pkmJson, i)
+ *              makeBig(this, allPokemons, i)
  *          } );
  *   What is the solution for this problem?
  * 
@@ -33,61 +33,74 @@
  * - pkms:  pokemons
  */
 
+let offset = 0;
+let allPokemons =[];
+
 async function loadpkms() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0';
+    let url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offset}`;
     let response = await fetch(url);
     let pkms = await response.json();
-    let results = pkms.results;
-    loadpkmCards(results)
-}
-
-function loadpkmCards(results) {
-    results.forEach(async (item, i) => {
+    for (let p = 0; p < pkms.results.length; p++) {
+        const item = pkms.results[p];
         let url = item.url
         let response = await fetch(url)
-        let pkmJson = await response.json()
-        renderpkmCards(pkmJson, i)
-    })
+        let pokemon = await response.json()
+        allPokemons.push(pokemon)
+    }
+    //////this forEach Methode doesn't work for an async function/////
+    // pkms.results.forEach(async (item) => {
+    //     let url = item.url
+    //     let response = await fetch(url)
+    //     let pokemon = await response.json()
+    //     allPokemons.push(pokemon)
+    // })
+    console.log(allPokemons);
+    document.getElementById('pkms').innerHTML = ``;
+    renderpkmCards();
 }
 
-function renderpkmCards(pkmJson, i) {
-    let name = pkmJson.name;
-    let front_default = pkmJson.sprites.other['official-artwork'].front_default;
-    let id = pkmJson.id;
-    let types = pkmJson.types;
-    let base_experience = pkmJson.base_experience;
-    let weight = pkmJson.weight;
-    let height = pkmJson.height;
-    document.getElementById('pkms').innerHTML += `
-        <div class="pkmCard bg-${types[0].type.name}" id="pkmCard(${i})" >
-            <div class="cardCover"  onclick="makeBig(${i})" id="cardCover(${i})""></div>
-            <div class="header">
-                <i class="backBtn fas fa-arrow-left" onclick="makeSmall(${i})" style="display:none"></i>
-                <h5 id="name">${name}</h5>
-                <div>#${id}</div>
-            </div>
-            <div class="img">
-                <img src='${front_default}'>
-            </div>
-            <div class="types" id="types(${i})"></div>
-            <div class="detail" style="display:none;">
-                <div class="text-center m-1">Base Experience ${base_experience}</div>
-                <div class="d-flex flex-row justify-content-evenly">
-                    <div>Weight ${weight}</div>
-                    <div>Height ${height}</div>
+function renderpkmCards() {
+    for (let i = 0; i < allPokemons.length; i++) {
+        let pkm = allPokemons[i];
+        let name = pkm.name;
+        let front_default = pkm.sprites.other['official-artwork'].front_default;
+        let id = pkm.id;
+        let types = pkm.types;
+        let base_experience = pkm.base_experience;
+        let weight = pkm.weight;
+        let height = pkm.height;
+        document.getElementById('pkms').innerHTML += `
+            <div class="pkmCard bg-${types[0].type.name}" id="pkmCard(${i})" >
+                <div class="cardCover"  onclick="makeBig(${i})" id="cardCover(${i})""></div>
+                <div class="header">
+                    <i class="backBtn fas fa-arrow-left" onclick="makeSmall(${i})" style="display:none"></i>
+                    <h5 id="name">${name}</h5>
+                    <div>#${id}</div>
                 </div>
-                <div id="pkmCardNav(${i})" class="pkmCardNav"></div>
-                <table id="abilities(${i})" class="abilities"></table>
-                <table id="stats(${i})" class="stats"></table>
-                <div id="evolution(${i})" class="evolution"></div>
-                <div id="moves(${i})" class="moves"></div>
+                <div class="img">
+                    <img src='${front_default}'>
+                </div>
+                <div class="types" id="types(${i})"></div>
+                <div class="detail" style="display:none;">
+                    <div class="text-center m-1">Base Experience ${base_experience}</div>
+                    <div class="d-flex flex-row justify-content-evenly">
+                        <div>Weight ${weight}</div>
+                        <div>Height ${height}</div>
+                    </div>
+                    <div id="pkmCardNav(${i})" class="pkmCardNav"></div>
+                    <table id="abilities(${i})" class="abilities"></table>
+                    <table id="stats(${i})" class="stats"></table>
+                    <div id="evolution(${i})" class="evolution"></div>
+                    <div id="moves(${i})" class="moves"></div>
+                </div>
             </div>
-        </div>
-    `;
-    loadTypes(types, i);
-    loadpkmCardNav(i);
-    loadStats(pkmJson, i);
-    document.getElementById(`pkmCard(${i})`).style.transition = "all 0.1s";
+        `;
+        loadTypes(types, i);
+        loadpkmCardNav(i);
+        loadStats(pkm, i);
+        document.getElementById(`pkmCard(${i})`).style.transition = "all 0.1s";
+        document.getElementById('loadingIcon').style = 'display: none';
+    }
 }
 
 function loadTypes(types, i) {
@@ -119,8 +132,8 @@ function activeMark(a){
     a.classList.add('active');
 }
 
-function loadStats(pkmJson, i){
-    pkmJson.stats.forEach(item =>{
+function loadStats(pkm, i){
+    pkm.stats.forEach(item =>{
         document.getElementById(`stats(${i})`).innerHTML +=`
             <tr>
                 <th class="statName">${item.stat.name}</th>
@@ -155,4 +168,12 @@ function makeSmall(i){
     });
 }
 
-function loadNextPokemons(){}
+window.onscroll = loadNext20Pkms;
+
+function loadNext20Pkms() {
+    if((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight){
+        document.getElementById('loadingIcon').style = 'display: block';
+        offset += 20;
+        loadpkms();
+    }
+}
